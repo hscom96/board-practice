5f8de90e41eee887f4265e51677fe47ca46d6be0
xxell8 <cell8pepper@gmail.com>
2022-02-18 18:47:03 +0900

    #9 feat: 로그인, 회원가입 폼을 담을 wrapper 생성

---
+++ frontend/src/components/user/UserForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="user-form">
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from './UserFormLogin'
+export default {
+  name: 'UserForm',
+  components: {
+    LoginForm,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .user-form {
+    width: 450px;
+    height: 700px;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    background-color: #fff;
+    box-shadow: 0px 2px 8px rgba($color-dark, 0.1);
+  }
+</style>
