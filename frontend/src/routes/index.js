e5881b2e1813e402f730778c67a5cba4d6b3c797
xxell8 <cell8pepper@gmail.com>
2022-02-20 13:26:05 +0900

    #11 feat: 회원가입 페이지 라우트 등록

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '~/views/Home'
 import Login from '~/views/user/Login'
+import Signup from '~/views/user/Signup'
 
 export default createRouter({
   history: createWebHistory(),
@@ -14,6 +15,11 @@ export default createRouter({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
     },
   ]
 })
