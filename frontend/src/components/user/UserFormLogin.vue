d202dc8040c71ce0ebb180e91dd28525dfb58871
xxell8 <cell8pepper@gmail.com>
2022-02-20 12:23:34 +0900

    #9 feat: 유효성 검증 로직 및 스타일 추가

--- frontend/src/components/user/UserFormLogin.vue
+++ frontend/src/components/user/UserFormLogin.vue
@@ -2,13 +2,15 @@
   <div class="login-form">
     <div class="input-group">
       <div class="input-box">
-        <input 
-          v-model="username"
-          type="text"
-          class="input"
-          autocapitalize="off"
-          placeholder="계정을 입력해 주세요."
-          required />
+        <div :class="[isChecked.username ? 'checked' : '', 'input']">
+          <i class="fa-solid fa-user-astronaut"></i>
+          <input 
+            v-model="username"
+            type="text"
+            autocapitalize="off"
+            placeholder="계정을 입력해 주세요."
+            required />
+        </div>
         <div
           v-if="error.username"
           class="error-text">
@@ -16,13 +18,15 @@
         </div>
       </div>
       <div class="input-box">
-        <input 
-          v-model="password"
-          type="password"
-          class="input"
-          autocapitalize="off"
-          placeholder="비밀번호를 입력해 주세요."
-          required />
+        <div :class="[isChecked.password ? 'checked' : '', 'input']">
+          <i class="fa-solid fa-key"></i>
+          <input 
+            v-model="password"
+            type="password"
+            autocapitalize="off"
+            placeholder="비밀번호를 입력해 주세요."
+            required />
+        </div>
         <div
           v-if="error.password"
           class="error-text">
@@ -44,6 +48,52 @@ export default {
         username: false,
         password: false
       },
+      isChecked: {
+        username: false,
+        password: false
+      },
+      complete: false
+    }
+  },
+  watch: {
+    username() {
+      this.checkForm()
+    },
+    password() {
+      this.checkForm()
+    },
+    complete() {
+      this.$emit('activate', this.complete)
+    }
+  },
+  methods: {
+    checkForm() {
+      this.checkUserName()
+      this.checkPassword()
+
+      let complete = true
+      Object.values(this.error).map(val => {
+        if (val) complete = false
+      })
+      this.complete = complete
+    },
+    checkUserName() {
+      if (this.username.trim().length >= 2) {
+        this.error.username = false
+        this.isChecked.username = true
+      } else {
+        this.error.username = '닉네임은 2자 이상이어야 합니다.'
+        this.isChecked.username = false
+      }
+    },
+    checkPassword() {
+      if (this.password.trim().length >= 8) {
+        this.error.password = false
+        this.isChecked.password = true
+      } else {
+        this.error.password = '비밀번호는 8자 이상이어야 합니다.'
+        this.isChecked.password = false
+      }
     }
   }
 }
@@ -55,25 +105,50 @@ export default {
   .input-box {
     position: relative;
     .input {
+      box-sizing: border-box;
       width: 300px !important;
       height: 40px;
+
       margin-top: 20px;
       background-color: #f1f1f1;
       color: $color-darkgrey;
-      box-shadow: none;
-      border: none;
-      border-bottom: 2px solid $color-primary;
-      outline: none;
+      border-bottom: 2px solid $color-grey;
       padding-left: 20px;
-      padding-right: 10px;
+      padding-right: 20px;
       font-size: 1rem;
-      transition: none;
+      display: flex;
+      align-items: center;
+      gap: 10px;
+
+      &.checked {
+        border-bottom: 2px solid $color-primary;
+      }
+
+      &:focus-within {
+        border-bottom: 2px solid $color-primary;
+      }
+
+      i {
+        font-size: 20px;
+        color: $color-grey;
+      }
+
+      input {
+        width: 100%;
+        height: 20px;
+        font-size: 1rem;
+        background: none;
+        box-shadow: none;
+        border: none;
+        outline: none;
+        transition: none;
+      }
     }
     .error-text {
       margin-top: 3px;
       padding-left: 20px;
-      color: $color-primary;
-      font-size: 1rem;
+      color: $color-grey;
+      font-size: .8rem;
       font-weight: 200;
     }
   }
