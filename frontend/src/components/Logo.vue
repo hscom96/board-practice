3a8bbe19de3f29de1ddc257c456a73123c22a3ac
xxell8 <cell8pepper@gmail.com>
2022-02-20 12:21:24 +0900

    #9 feat: 로고 컴포넌트 생성

---
+++ frontend/src/components/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="logo">
+    <Logo11st />
+    <div class="text">
+      중고거래
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo11st from '~/assets/11st.svg'
+
+export default {
+  name: 'Logo',
+  components: {
+    Logo11st
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    .text {
+      font-size: 30px;
+      font-weight: 700;
+      color: $color-grey;
+    }
+  }
+</style>
