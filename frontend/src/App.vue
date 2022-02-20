6c41e6164865b9699492eed3c9de12355f130554
xxell8 <cell8pepper@gmail.com>
2022-02-20 12:22:35 +0900

    #9 design: 기본 폰트 전역 적용

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,4 +14,9 @@ export default {
 
 <style lang="scss">
   @import "./scss/_variables.scss";
+  #app {
+    font-family: 'Noto Sans KR', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
