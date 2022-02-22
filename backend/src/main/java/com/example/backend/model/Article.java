e7ca6328ff200f17a12ba182bbfd7f896e505206
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-22 16:36:27 +0900

    feat: Article 엔티티 클래스 구현

---
+++ backend/src/main/java/com/example/backend/model/Article.java
@@ -0,0 +1,59 @@
+package com.example.backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class Article extends AuditProperties {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id", nullable = false)
+    private Long articleId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Column(name = "like_cnt")
+    private int likeCnt;
+
+    @Column(name = "sad_cnt")
+    private int sadCnt;
+
+    @Column(name = "upset_cnt")
+    private int upsetCnt;
+
+    @Column(name = "view_cnt")
+    private int viewCnt;
+
+    @Column(name = "label")
+    private String label;
+
+    @Column(name = "comment_cnt")
+    private int commentCnt;
+}
+
+
