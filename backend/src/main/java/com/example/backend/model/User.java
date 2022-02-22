8835a63a905756ac01f3a45c85854727636d676f
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-22 16:36:11 +0900

    feat: User 엔티티 클래스 구현

---
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -0,0 +1,44 @@
+package com.example.backend.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User extends AuditProperties {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private long userId;
+
+    @Column(name ="name", nullable = false)
+    private String name;
+
+    @Column(name ="user_role", nullable = false)
+    private String userRole;
+
+    @Column(name ="password", nullable = false)
+    private String password;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Column(name="last_login_at",nullable = false)
+    private LocalDateTime lastLoginAt;
+
+}
