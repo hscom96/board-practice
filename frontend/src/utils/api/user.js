2ef3642448266fdc7c2e691552224ce0c30c8e96
xxell8 <cell8pepper@gmail.com>
2022-02-24 14:17:27 +0900

    #30 feat: axios 요청 파트 분리

---
+++ frontend/src/utils/api/user.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const SERVER_URL = 'http://localhost:8080'
+
+export default {
+  login(userData) {
+    return axios({
+      url: SERVER_URL + '/member/login',
+      method: 'post',
+      data: userData
+    })
+  },
+  signup(userData) {
+    return axios({
+      url: SERVER_URL + '/member/register',
+      method: 'post',
+      data: userData
+    })
+  },
+}
