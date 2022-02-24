ce1bb76433d69611328ac1f02da7eb02323bbd91
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-24 18:18:23 +0900

    feat: CommentDto 내 시간 관련 meta data를 TimeUtil 클래스를 가져와 활용

--- backend/src/main/java/com/example/backend/dto/CommentDto.java
+++ backend/src/main/java/com/example/backend/dto/CommentDto.java
@@ -1,9 +1,8 @@
 package com.example.backend.dto;
 
+import com.example.backend.common.util.Timeutil;
 import com.example.backend.dto.common.CommentProperties;
 import com.example.backend.model.Comment;
-import com.example.backend.model.User;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,10 +17,10 @@ public class CommentDto extends CommentProperties {
     private Long articleId;
     private String content;
 
-    private LocalDateTime createdAt;
+    private Long createdAt;
     private Long createdById;
 
-    private LocalDateTime modifiedAt;
+    private Long modifiedAt;
     private Long modifiedById;
 
     public static CommentDto from(Comment comment) {
@@ -30,16 +29,9 @@ public class CommentDto extends CommentProperties {
             .parentId(comment.getParentId())
             .articleId(comment.getArticleId())
             .content(comment.getContent())
-            .createdAt(comment.getCreatedAt())
+            .createdAt(Timeutil.convertToTimestamp(comment.getCreatedAt()))
+            .modifiedAt(Timeutil.convertToTimestamp(comment.getModifiedAt()))
             .createdById(comment.getCreatedById())
-            .modifiedAt(comment.getModifiedAt())
             .modifiedById(comment.getModifiedById()).build();
-    }
-
-    public static UserDto from(User user){
-        return UserDto.builder()
-            .userId(user.getUserId())
-            .name(user.getName())
-            .userName(user.getUserName()).build();
     }
 }
