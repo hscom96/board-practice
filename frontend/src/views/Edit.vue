97bab4d0595a8d5ff4499cb0337915280493f8d9
xxell8 <cell8pepper@gmail.com>
2022-02-24 18:38:40 +0900

    #60 feat: 게시글 수정 페이지 작성 및 라우팅

---
+++ frontend/src/views/Edit.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="update">
+    <Header />
+    <div class="content">
+      <Form />
+    </div>
+    <Footer class="fix-bottom" />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header'
+import Footer from '~/components/Footer'
+import Form from '~/components/write/WriteForm'
+
+export default {
+  name: 'Edit',
+  components: {
+    Header,
+    Form,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .update {
+    .fix-bottom {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+    }
+  }
+</style>
