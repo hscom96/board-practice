6bd88e79881014af2c6fb50f982d8edd4410d62b
한승진 <gkstmdwls100@naver.com>
2022-02-24 18:24:45 +0900

    feat: timestamp 출력 정상처리

--- frontend/src/components/article_detail/ArticleDetail.vue
+++ frontend/src/components/article_detail/ArticleDetail.vue
@@ -22,7 +22,7 @@
               <span>{{ articleInfo.created_by }}</span>
             </div>
             <div class="time-info">
-              <span>{{ articleInfo.created_at }}</span>
+              <span>{{ convertTime(articleInfo.created_at) }}</span>
               <span v-if="isModified(articleInfo.created_at, articleInfo.modified_at)">(수정됨)</span>
               ·
               <span>조회 {{ articleInfo.view_cnt }}</span>
@@ -103,6 +103,17 @@ export default {
     },
     onClickArticleListButton() {
       this.$router.push('/article')
+    },
+    convertTime(timestamp) {
+      const date = new Date(timestamp)
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1
+      const day = date.getDate()
+      const hour = date.getHours()
+      const min = date.getMinutes()
+      const sec = date.getSeconds()
+
+      return `${year}.${month < 10 ? '0'+month : month}.${day < 10 ? '0'+day : day} ${hour < 10 ? '0'+hour : hour}:${min < 10 ? '0'+min : min}:${sec < 10 ? '0'+sec : sec}`
     }
   },
 }
