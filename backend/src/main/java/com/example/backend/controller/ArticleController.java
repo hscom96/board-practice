0fcb04a5635f61c636db66a362ee52297c58b772
Jiin Lee <loveted0225@naver.com>
2022-02-24 16:26:52 +0900

    feat: ArticleController에 게시글 수정 메소드 추가

--- backend/src/main/java/com/example/backend/controller/ArticleController.java
+++ backend/src/main/java/com/example/backend/controller/ArticleController.java
@@ -4,11 +4,13 @@ import com.example.backend.common.constants.ResponseCode;
 import com.example.backend.common.resolver.CurrentUser;
 import com.example.backend.dto.ArticleDto;
 import com.example.backend.dto.common.CommonResponse;
+import com.example.backend.dto.request.ArticleUpdateRequest;
 import com.example.backend.dto.request.ArticleWriteRequest;
 import com.example.backend.service.ArticleService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +30,13 @@ public class ArticleController {
         ArticleDto articleDto = articleService.write(articleWriteRequest, currentUserId);
         return ResponseEntity.ok(new CommonResponse<>(ResponseCode.SUCCESS, articleDto));
     }
+
+    @PostMapping("/{articleId}")
+    public ResponseEntity<CommonResponse<ArticleDto>> update(@PathVariable("articleId") Long articleId,
+        @RequestBody @Valid ArticleUpdateRequest articleUpdateRequest,
+        @CurrentUser Long currentUserId){
+       ArticleDto articleDto = articleService.update(articleId, articleUpdateRequest, currentUserId);
+       return ResponseEntity.ok(new CommonResponse<>(ResponseCode.SUCCESS,articleDto));
+    }
+
 }
