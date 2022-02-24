5f8ead100213c8e7561683f7787939cc8f1fe0de
Jiin Lee <loveted0225@naver.com>
2022-02-24 17:59:09 +0900

    feat: Article 삭제 메소드 추가

--- backend/src/main/java/com/example/backend/controller/ArticleController.java
+++ backend/src/main/java/com/example/backend/controller/ArticleController.java
@@ -10,6 +10,7 @@ import com.example.backend.service.ArticleService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,25 +36,33 @@ public class ArticleController {
     }
 
     @PutMapping("/{articleId}")
-    public ResponseEntity<CommonResponse<ArticleDto>> update(@PathVariable("articleId") Long articleId,
+    public ResponseEntity<CommonResponse<ArticleDto>> update(
+        @PathVariable("articleId") Long articleId,
         @RequestBody @Valid ArticleUpdateRequest articleUpdateRequest,
-        @CurrentUser Long currentUserId){
-       ArticleDto articleDto = articleService.update(articleId, articleUpdateRequest, currentUserId);
-       return ResponseEntity.ok(new CommonResponse<>(ResponseCode.SUCCESS,articleDto));
+        @CurrentUser Long currentUserId) {
+        ArticleDto articleDto = articleService.update(articleId, articleUpdateRequest,
+            currentUserId);
+        return ResponseEntity.ok(new CommonResponse<>(ResponseCode.SUCCESS, articleDto));
     }
 
     @GetMapping("/{articleId}")
-    public ResponseEntity<CommonResponse<ArticleDto>> getArticle(@PathVariable("articleId") Long articleId){
+    public ResponseEntity<CommonResponse<ArticleDto>> getArticle(
+        @PathVariable("articleId") Long articleId) {
         ArticleDto articleDto = articleService.getArticle(articleId);
         return ResponseEntity.ok(new CommonResponse<>(ResponseCode.SUCCESS, articleDto));
     }
 
     @GetMapping
-    public ResponseEntity<CommonResponse<?>> getArticles(@RequestParam("page") int page, @RequestParam("size") int size){
+    public ResponseEntity<CommonResponse<?>> getArticles(@RequestParam("page") int page,
+        @RequestParam("size") int size) {
         var articles = articleService.getArticles(page, size);
         return ResponseEntity.ok(new CommonResponse<>(ResponseCode.SUCCESS, articles));
     }
 
-
-
+    @DeleteMapping("/{articleId}")
+    public ResponseEntity<CommonResponse<ArticleDto>> delete(
+        @PathVariable("articleId") Long articleId) {
+        articleService.delete(articleId);
+        return ResponseEntity.ok(new CommonResponse<>(ResponseCode.SUCCESS));
+    }
 }
