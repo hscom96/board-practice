b8032122eb980f9e539ca11eac791432f39830a1
xxell8 <cell8pepper@gmail.com>
2022-02-24 14:18:52 +0900

    #30 style: user store 구조 변경

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,6 +1,39 @@
+import userApi from '~/utils/api/user.js'
+
+const state = {
+  userId: null,
+  username: ''
+}
+
+const actions = {
+  async onLogin({ commit }, userData) {
+    try {
+      const { data } = await userApi.login(userData)
+      console.log(data)
+      commit()
+    } catch (err) {
+      console.log(err.response)
+    }
+  }
+}
+
+const mutations = {
+  SET_USER_ID(state, payload) {
+    state.userId = payload
+  },
+  SET_USERNAME(state, payload) {
+    state.username = payload
+  }
+}
+
+const getters = {
+
+}
+
 export default {
   namespaced: true,
-  state: () => ({
-    
-  })
+  state,
+  actions,
+  mutations,
+  getters
 }
